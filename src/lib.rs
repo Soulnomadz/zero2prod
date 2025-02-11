29a1f3b866f9061c5dcd8aec739a45b68afaeab7
oywzzc <oyj7789@163.com>
2025-02-11 15:43:58 +0800

    去除多余代码

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,6 @@
 use actix_web::{web, App, HttpRequest, HttpServer, Responder, HttpResponse};
 use actix_web::dev::Server;
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
@@ -13,25 +8,10 @@ async fn health_check() -> HttpResponse {
 pub fn run() -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
 	    .route("/health_check", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
     })
     .bind("0.0.0.0:8000")?
     .run();
-    //.await
 
     Ok(server)
 }
-
-//#[cfg(test)]
-//mod tests {
-//  use crate::health_check;
-//
-//  #[tokio::test]
-//  async fn health_check_succeeds() {
-//    let response = health_check().await;
-//
-//    assert!(response.status().is_success())
-//  }
-//}
